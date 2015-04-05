4ef093f8353f5bfb2ad5efad090f26e2ab91b587
stefanpante <stefanpante@outlook.com>
2015-04-06 00:41:37 +0200

    Get more information about drivers

---
+++ js/loaderanimation.js
@@ -0,0 +1,15 @@
+//function LoaderAnimation(element){
+//    this.element = element;
+//}
+//
+//LoaderAnimation.prototype = {
+//    html:     '<div id="loader"><div class="loader"><div class="bar one"></div><div class="bar two"></div><div class="bar three"></div><div class="bar four"></div><div class="bar five"></div></div></div>',
+//    function: startAnimation(){
+//        $(element).append(html);
+//    },
+//        
+//    function: stopLoader(){
+//        $("#loader").remove();
+//    } 
+//    
+//}   
