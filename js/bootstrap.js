2b0b45cf75cd8e6b1925e35ca9d673b14c2982c9
stefanpante <stefanpante@outlook.com>
2015-05-18 00:27:44 +0200

    Fixing

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,17 +3,18 @@ $(document).ready(function () {
 
 });
 
-$(".changedriver").click(function (event) {
-    var driver = $(event.target).data("driver");
-    var name = $(event.target).text();
-    $("#title .name").text(name);
-    $("#wrap_timeline").empty();
-    $("#wrapperSVG").remove();
-    $("#wrapperSVGMINI").remove();
-    $("#timelineNav .year").remove();
-    makeBarCharts(window.dat1, oldestDriver, driver);
-    oldestDriver = driver;
-});
+//$(".changedriver").click(function (event) {
+//    var driver = $(event.target).data("driver");
+//    var name = $(event.target).text();
+//    //$("#title .name").text(name);
+//    $("#wrap_timeline").empty();
+//    alert("test");
+//    $("#wrapperSVG").remove();
+//    $("#wrapperSVGMINI").remove();
+//    $("#timelineNav .year").remove();
+//    makeBarCharts(window.dat1, oldestDriver, driver);
+//    oldestDriver = driver;
+//});
 
 $("#pointSelector").off("click");
 $("#pointSelector").on("click", function () {
@@ -42,5 +43,7 @@ $(".switch").click(function (event) {
     $("#wrapperSVG").remove();
     $("#wrapperSVGMINI").remove();
     $("#timelineNav .year").remove();
+    $("#title .name.two").text("Mario Andretti");
+        $("#title .name.one").text("Niki Lauda");
     makeBarCharts(window.dat1,"mario_andretti", "lauda");
 })
